28eebad51b5db30fad240f1cd9f3e1b0832e71bd
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-11-05 09:53:14 -0800

    initial thoughts

---
+++ towerOfHanoi.js
@@ -0,0 +1,16 @@
+function towerOfHanoi(arr) {
+  const rodHeight = arr[0].length;
+  const rodA = arr[0];
+  const rodB = arr[1];
+  const rodC = arr[2];
+  if (rodC.length === rodHeight) {
+    return;
+  }
+}
+
+number - number of disc 
+
+arr [['-', '--', '---', '----'],[],[]]
+
+arr [['----', '---', '--', '-']]
+
