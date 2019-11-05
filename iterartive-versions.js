33fb051fb02cd774b749333137d0f667b056a375
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-11-05 12:20:28 -0800

    complete power calculator

--- iterartive-versions.js
+++ iterartive-versions.js
@@ -7,4 +7,18 @@ function countSheep(num){
   }
   console.log('All sheep jumped over the fence');
 }
-console.log(countSheep(3));
+//console.log(countSheep(3));
+
+//Q2: Power Calculator
+function powerCalculator(base, exponent) {
+  let accumulator = 1;
+  if (exponent < 0) {
+    console.log('Exponent should be >= 0')
+  }
+  for (let i = 0; i < exponent; i++) {
+    accumulator = base*accumulator;
+  }
+  console.log(accumulator);
+}
+
+// powerCalculator(5, 2);
